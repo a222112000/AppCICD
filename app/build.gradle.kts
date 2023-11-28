535dc7abd7b96ff60ff1c5a42eb22f64b6eea6f0
Alselwi <a222112000@yahoo.com>
2023-11-28 08:26:35 +0000

    tragetsdk changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.tourism.appcicd"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.tourism.appcicd"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
